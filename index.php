d506d9d2f2f3c36f7ec10ada7d54b41f0324da29
Barkentine <B@example.com>
2017-12-29 16:04:49 +0100

    Minor changes to the index file.

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php header('Content-Type: text/html; charset=utf-8'); ?>
-
 <?php
     include("View/v_header.php");
 
+    // If action is not defined set it to *accueil*.
     if(!isset($_REQUEST['action']))
     {
         $action = 'accueil';
@@ -11,9 +11,12 @@
     {
         $action = $_REQUEST['action'];
     }
-    switch ($action) {
+
+    switch ($action)
+    {
         //case 'value':
-        case 'value':
+        case 'accueil':
+            include("View/v_homepage.php");
             break;
         default:
             break;
