e07c62e5cfd7a156c7854abaf2d8ad67262e7587
Barkentine <B@example.com>
2017-12-29 18:14:42 +0100

    Add index from View as the Controller.

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php header('Content-Type: text/html; charset=utf-8'); ?>
+<?php
+    include("View/v_header.php");
+
+    // If action is not defined set it to *accueil*.
+    if(!isset($_REQUEST['action']))
+    {
+        $action = 'accueil';
+    }
+    else
+    {
+        $action = $_REQUEST['action'];
+    }
+
+    switch ($action)
+    {
+        //case 'value':
+        case 'accueil':
+            include("View/v_homepage.php");
+            break;
+        case 'vols'
+            include("View/v_vols.php");
+            break;
+        default:
+            break;
+    }
+    include("View/v_footer.php") ;
+?>
+
