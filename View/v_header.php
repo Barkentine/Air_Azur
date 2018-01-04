f9e8a8cefe3e789bfab9881687692a803be80fff
Barkentine <B@example.com>
2018-01-04 16:31:25 +0100

    Change spaces to tabs.

--- View/v_header.php
+++ View/v_header.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <title>Air-Azur — Intranet</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="../css/bootstrap.min.css">
+	<title>Air-Azur — Intranet</title>
 </head>
 <body>
 <div class="container">
-    <nav class="navbar navbar-expand-sm bg-light">
-    <a class="navbar-brand" href="#">Air Azur</a>
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="#">Catalogue des vols</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Voir les réservations</a>
-            </li>
-        </ul>
-    </nav>
+	<nav class="navbar navbar-expand-sm bg-light">
+	<a class="navbar-brand" href="#">Air Azur</a>
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link" href="#">Catalogue des vols</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Voir les réservations</a>
+			</li>
+		</ul>
+	</nav>
--- View/v_homepage.php
+++ View/v_homepage.php
@@ -1,4 +1,4 @@
 <div class="row">
-    <div class="col">
-        Bienvenue sur l'intranet d'Air Azur.
-    </div>
+	<div class="col">
+		Bienvenue sur l'intranet d'Air Azur.
+	</div>
